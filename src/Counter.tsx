514118751c1fba43e22426c75742aa96e77db64a
yuuki-katsuta <reptpr0314@gmail.com>
2020-12-28 17:18:19 +0900

    Introduction to useEffect and useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 //useStateを使う場合は、型引数を指定する。それによりuseStateが返す関数が型制約をもつ
 
 const Couter: React.FC<{}> = () => {
@@ -14,11 +14,40 @@ const Couter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  //値はcurrentというメンバ名で参照できる(renderTimes.current)
+  //useRefにも型引数を指定できる
+
+  //render毎に実行
+  useEffect(() => {
+    renderTimes.current += 1;
+  });
+
+  const ref = useRef<HTMLInputElement>(null!);
+  //refをRefObject型にするために、useRefに型引数<HTMLInputElement>を指定する
+  //inputのrefに指定した場合、そのinput要素に対する参照をもつ
+
+  const focusInput = () => {
+    //useRefをnullで初期化しているので、ref.currentがnullではなかった場合適用させる
+    //ref.currentはinput要素に対する参照を保持する
+    // const current = ref.current;
+    // if (current != null) current.focus();
+
+    //current?でnullの場合はfocusの実行をしない
+    //ref.current?.focus();
+
+    //useRef<HTMLInputElement>null!とすることで、その直前のデータ型はnull型ではないということをコンパイラに知らせることができる(null!の!をNon-Null Assertion Operatorという)
+    ref.current?.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current}</div>
+      <input ref={ref} type='text' />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
