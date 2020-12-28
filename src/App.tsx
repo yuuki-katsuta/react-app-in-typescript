f9f946d496a3955c674e054eeae23c06122b6895
yuuki-katsuta <reptpr0314@gmail.com>
2020-12-28 13:52:52 +0900

    Introduction to Functional Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+//Functionコンポーネントはを使う場合、必ず関数に対してアノテーションをしてジェネリクス型でpropsの型を指定する
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+interface AppProps {
+  message?: string
+}
+
+//React.FunctionComponentでコンポーネントに対する型制約が可能。また、型引数を受け取れる
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
+  //Typescriptでpropsのアノテーションするには、{ message }: { message: string }のように記述。これで、渡されるpropsはmessageのみ、その型はstring型となる
+  return <div>{message}</div>;
 };
+
+App.defaultProps = {
+  //外からmessageを受け取らなかった場合のデフォルトのpropsを指定できる
+  message: 'Hell, defaultProps!'
+}
 
 export default App;
